f34471496c15827aed494c67c6994d49348385c7
henrikusmaja <henrikusmaja@users.noreply.github.com>
2026-01-28 22:46:08 +1100

    Submit 6
    Changing the summary or the title from "Day" to "Submit". Learning and adding a few functions in script.js to show the message if we click the contacts and also to reload the page when we click the restartIcon.

---
+++ script.js
@@ -0,0 +1,20 @@
+const contacts = document.querySelectorAll('.whadupContact');
+const message = document.querySelector('.chatHenry');
+const refreshButton = document.querySelector('.restartIcon');
+
+
+// to show the chat
+contacts.forEach(contact => {
+    contact.addEventListener("click", chatAppearsFunc, true);
+});
+
+function chatAppearsFunc() {
+    message.classList.remove('hidden');
+}
+
+// to refresh the page
+refreshButton.addEventListener("click", refreshPageFunc, true);
+
+function refreshPageFunc() {
+    window.location.reload();
+}
